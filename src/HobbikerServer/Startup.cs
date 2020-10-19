b7d7fc1e89dc2e46860c28252d2d472238a8a953
Davlet Dzhakishev <davlet@fjong.com>
2020-10-19 17:52:11 +0200

    remove unused configuration

--- src/HobbikerServer/Startup.cs
+++ src/HobbikerServer/Startup.cs
@@ -14,12 +14,12 @@ namespace HobbikerServer
     {
         public Startup(IHostingEnvironment env)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            // var builder = new ConfigurationBuilder()
+            //     .SetBasePath(env.ContentRootPath)
+            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //     .AddEnvironmentVariables();
+            // Configuration = builder.Build();
         }
 
         public IConfigurationRoot Configuration { get; }
