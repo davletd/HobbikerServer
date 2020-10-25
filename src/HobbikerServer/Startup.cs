7d18d0ce44b7de4f3949fe10d575f168e675d63c
Davlet Dzhakishev <davlet@fjong.com>
2020-10-25 20:56:23 +0100

    update envs

--- src/HobbikerServer/Startup.cs
+++ src/HobbikerServer/Startup.cs
@@ -14,7 +14,7 @@ namespace HobbikerServer
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IWebHostEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -37,7 +37,7 @@ namespace HobbikerServer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             // loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             // loggerFactory.AddDebug();
